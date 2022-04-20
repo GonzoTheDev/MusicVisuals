8ee8fe8d3861e08ab96a731228c9535bd15c53c6
ignasprak <91851842+ignasprak@users.noreply.github.com>
2022-04-20 19:01:39 +0100

    uhohtroubleindowntown

--- java/src/c20703429/IgnasVisual.java
+++ java/src/c20703429/IgnasVisual.java
@@ -14,24 +14,38 @@ public class IgnasVisual extends Visual {
 
     public void render() {
         mv.background(20);
-        mv.noStroke();
-        mv.ellipseMode(RADIUS);
+        mv.colorMode(RGB, 255);
+        // mv.noStroke();
+        // mv.ellipseMode(RADIUS);
         
         //do stuff here
 
         mv.calculateAverageAmplitude(); 
         float r = mv.getSmoothedAmplitude();
 
-        r = map(r, 0, 40, 0, 20);
+        // r = map(r, 0, 40, 0, 20);
 
-        int midX = mv.width/2;
-        int midY = mv.height/2;
+        // int midX = mv.width/2;
+        // int midY = mv.height/2;
 
-        mv.circle(midX, midY, map(r, 0, 1, 0, 1000)); //how do I make this run around in a curved ellipse
+        // mv.circle(midX, midY, map(r, 0, 1, 0, 1000)); //how do I make this run around in a curved ellipse
+        // println(r);
+        // mv.fill(200);
+        // for (int i = 0; i < 700; i++) {
+        //     for (int j = 0; j < 1400; j++) {
+        //         mv.line(700, 400, i, j); //x1 and y1 will be static but we want x2 and y2 to extend to the edge of the application 
+        //     }
+        // }
+        int i = 0;
+        int j = 0;
+        for(i = 0; i < 1400; i = i + 20) //last condition controls the amount of lines dependant on the second condition
+        {
+            for(j = 0; j < 800; j = j + 20) //last condition controls the amount of lines dependant on the second condition
+        {
+            mv.stroke(r, r, r);
+            mv.line(700, 1400, i, j); //x1 and y1 will be static but we want x2 and y2 to extend to the edge of the application 
+        }
+        }
         println(r);
-
-        //if it touches the edge thats the max it can go
-        
-
     }
 }
