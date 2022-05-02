262644d6fa444d2025f8952662d8a8e5e6152db6
Adam O'Shea <C20372181@mytudublin.ie>
2022-05-02 03:15:11 +0100

    somthing

--- java/src/c20703429/AdamVisual.java
+++ java/src/c20703429/AdamVisual.java
@@ -3,73 +3,96 @@ package c20703429;
 import ie.tudublin.*;
 import processing.core.PApplet;
 
-
-
-
 public class AdamVisual extends Visual {
 
     MainVisual mainVisual;
-    
 
     public AdamVisual(MainVisual mainVisual) {
         this.mainVisual = mainVisual;
     }
 
-    float colour1 = random(0, 255);
-    float colour2 = random(0, 255);
-    float colour3 = random(0, 255);
-    float bgC1, bgC2, bgC3 = 0;
+    float[] clrArr = new float[]{random(0, 255),random(0, 255),random(0, 255)};
+   
+    float[] prevClrs = new float[]{0,0,0};
+
     float eRadius = 20;
     int snareCnt = 0;
+    int unlock = 0;
+    int option = 0;    
+
+    public float[] reassignColours(float[] clrArr)
+    {
+        float[] colours = new float[]{random(0, 255),random(0, 255),random(0, 255)};
+        for(int i=0;i<3;i++)
+        {
+            clrArr[i] = colours[i];
+        }
+        return clrArr;
+    }
+    
+    
+
     public void render() {
         mainVisual.background(0, 0, 0);
         mainVisual.textSize(20);
         mainVisual.fill(255);
         mainVisual.textAlign(CENTER,CENTER);
-                                                            //mouseX affects size, mouseY affects colours, think of other things you uncreative fuck
 
         mainVisual.beat.detect(mainVisual.as.mix);
         mainVisual.beat.detectMode(0);
-        //float a = map(mainVisual.eRadius, 20, 80, 60, 255);
-        mainVisual.fill(colour1, colour2, colour3, 20);
+        mainVisual.fCounter++;
         
 
-        if (mainVisual.beat.isSnare())
+        println(mainVisual.adamOption);
+
+        if (mainVisual.beat.isKick())
         {
-            snareCnt++;            
+            snareCnt++;   
         }
-        if ( mainVisual.eRadius > ((float)mainVisual.width)/2 ) 
+
+        mainVisual.fill(clrArr[0], clrArr[1], clrArr[2], 50);
+        mainVisual.eRadius = 5*snareCnt;
+            
+        if (mainVisual.adamOption == 0)
         {
-            bgC1 = colour1;
-            bgC2 = colour2;
-            bgC3 = colour3;
-            colour1 = random(0, 255);
-            colour2 = random(0, 255);
-            colour3 = random(0, 255);
-            snareCnt = 0;
-            mainVisual.eRadius = 20;
-        }
-        else
-        {
-            mainVisual.eRadius = 25*snareCnt;
-            System.out.println(snareCnt);
+            // mainVisual.fill(clrArr[0], clrArr[1], clrArr[2], 50);
+               // mainVisual.eRadius = 5*snareCnt;
             mainVisual.ellipse(mainVisual.width/2, mainVisual.height/2, mainVisual.eRadius, mainVisual.eRadius);
+            if ( mainVisual.eRadius > ((float)mainVisual.width)/2 ) 
+            {
+                for (int i=0; i < 3; i++)
+                {
+                    prevClrs[i] = clrArr[i];
+                }
+                reassignColours(clrArr);
+                snareCnt = 0;
+                mainVisual.eRadius = 20;
+                
+            }
+        }
+        if (mainVisual.adamOption == 1)
+        {
+            for (int i = 0; i < 200; i += 20) 
+            {
+                mainVisual.bezier((float) mainVisual.width/2, (float)mainVisual.height/2, (float)mainVisual.eRadius, (float)mainVisual.eRadius, (float)mainVisual.eRadius/2, (float)mainVisual.eRadius/2,  (float)mainVisual.eRadius/2,  (float)mainVisual.eRadius/2);
+                mainVisual.bezier((float) mainVisual.width/2, (float)mainVisual.height/2, (float)mainVisual.eRadius, (float)mainVisual.eRadius, (float)mainVisual.eRadius/2, (float)mainVisual.eRadius/2,  (float)mainVisual.eRadius/2,  (float)mainVisual.eRadius/2);
+            }
             
         }
-        mainVisual.eRadius -= 5;
-        mainVisual.fill(bgC1, bgC2, bgC3, 30);
+        
+        mainVisual.fill(prevClrs[0], prevClrs[1], prevClrs[2], 25);
         mainVisual.rect(0, 0, mainVisual.height*2, mainVisual.width*2);
 
-                
+        float space = mainVisual.getSmoothedAmplitude();       
         mainVisual.stroke(PApplet.map(mainVisual.getAmplitude(), 0, 1, 0, 255), 255, 255);
         mainVisual.translate(mainVisual.width/2, mainVisual.height/2);
-        float gap = 1*mainVisual.getSmoothedAmplitude();
         for (float i = 0; i < mainVisual.width; i+=0.5 ) 
-        {
-            float x = (i * PApplet.cos(i)*gap);
-            float y = (-i * PApplet.sin(i))*gap;
+        { 
+            float x = (PApplet.cos(i)*space*i);
+            float y = (PApplet.sin(i)*space*i);
             mainVisual.point(x, y);
         }
+
         
     }
     
