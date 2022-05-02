78ac0c197c192794a76648cc8bc20e59e5b75d41
ignasprak <91851842+ignasprak@users.noreply.github.com>
2022-05-02 23:23:39 +0100

    happy extension day
    ;)

--- java/src/c20703429/IgnasVisual.java
+++ java/src/c20703429/IgnasVisual.java
@@ -16,47 +16,66 @@ public class IgnasVisual extends Visual {
     int orbit = 300;
     int angle = 270; //where the first circle starts from the bottom
     int angle2 = 90; //where the second circle starts from the bottom
-    ddf.minim.analysis.BeatDetect beat = BeatDetect();
+    boolean check1 = false; //this will be used to check once the second part of the song starts
 
     public void render() {
 
         //setup
         mv.background(20);
-        mv.colorMode(RGB, 255);      
-        mv.calculateAverageAmplitude();
-        
-        //initialisation 
-        float r = mv.getSmoothedAmplitude();
-        float r2 = map(r, 0, 40, 50, 1000);
-        int midX = mv.width/2;
+        mv.colorMode(HSB);      
+        mv.getFFT();        
+        float[] sb = mv.getSmoothedBands();
+
+        //initialisation
+            //moving bits
+                float r = mv.getSmoothedAmplitude();
+                float r2 = map(r, 0, 1 , 0, 40);
+                //bass frequencies
+                float ba = map(sb[0], 0, 50, 0, 200);
+                //low-mid frequencies
+                float lm = map(sb[1], 0, 400, 0, 400 ); 
+                //mid frequencies
+                float mi = sb[2];  
+                //high frequencies
+                float hi = sb[3]; 
+                //super high frequencies
+                float hh = sb[4]; 
+        //basics
+        int midX = mv.width/2; 
         int midY = mv.height/2;
+        int orbit = 300;
 
         //first fill
-        mv.fill(10);
+        mv.fill(10); 
         mv.stroke(153);
 
-        //this is the circle in the middle
-        mv.circle(midX, midY, map(r, 0, 1, 0, 1000)); 
+        //this is the circle in the middle, I want it to only put out the bass frequencies
+        mv.circle(midX, midY, lerp(map(ba, 0, 50, 0, 200), 100, (float) 0.5)); 
 
-        //beat detection
-        // if(mv.isBeat() == true)
-        // {
-
-        // }
+        //this will be the second circle, I want it to be smaller and have it put out the average amplitude
+        mv.fill(100);
+        mv.circle(midX, midY, r2 * 6); 
         
         //testing purposes
-        println(r + ", " + r2);
+        // println(mv.getSmoothedBands().length);
+        println("spin: " + r + ", mapped spin: " + r2 + ", orbit: " + orbit + "\n\nBA: " + sb[0] + ", LM: " + sb[1] + ", MI: "  + sb[2] + ", HI: "  + sb[3] + ", HH: "  + sb[4] + "\n\nmBA: " + ba + ", mLM: " + lm + ", mMI: "  + mi + ", mHI: "  + hi + ", mHH: "  + hh + "\n\n");
 
-        // circles circling
-        mv.fill(50);
+        //this is to make the circles change colour based on the music
+        mv.fill(map(lm, 0, 430, 0, 255), map(mi, 0, 3200, 0, 255), map(hi, 0, 4500, 0, 255));
+
         mv.noStroke();
+
+        // if(check1 == true)
+        // {
+        // circles circling
+            
             //first circle
             mv.push();
 
                 mv.translate(700 + (cos(radians(angle)) * orbit), 400 + (sin(radians(angle)) * orbit));
                 mv.rotate(radians(angle));
 
-                mv.circle(0, 0, r2);
+                mv.circle(0, 0, r2 * (map( 5 , 0, lerp(r, r2, 10), 1, 10)) * 3);
 
             mv.pop();
 
@@ -66,7 +85,7 @@ public class IgnasVisual extends Visual {
                 mv.translate(700 + (cos(radians(angle2)) * orbit), 400 + (sin(radians(angle2)) * orbit));
                 mv.rotate(radians(angle2));
 
-                mv.circle(0, 0, r2);
+                mv.circle(0, 0, r2 * (map( 5 , 0, lerp(r, r2, 10), 1, 10)) * 3);
 
             mv.pop();
 
@@ -82,8 +101,14 @@ public class IgnasVisual extends Visual {
             {
                 angle2 = 0;
             }
+    // }
+    
+        
 
-        //line that goes around main circle
+        //arc that goes around main circle
+        
+
+
 
     }
 }
