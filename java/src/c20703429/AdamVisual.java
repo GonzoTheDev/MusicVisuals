6fa670eefcb6eadedaac21b928f3516e61773fb1
Adam O'Shea <C20372181@mytudublin.ie>
2022-05-04 17:39:25 +0100

    commenting

--- java/src/c20703429/AdamVisual.java
+++ java/src/c20703429/AdamVisual.java
@@ -11,12 +11,14 @@ public class AdamVisual extends Visual {
         this.mainVisual = mainVisual;
     }
 
+    //Arrays for setting starting colours of shapes and background respectively
     float[] clrArr = new float[]{random(0, 255),random(0, 255),random(0, 255)};
     float[] prevClrs = new float[]{0,0,0};
 
-    float eRadius = 20;
+    //Initalise variable for measuring beats in song
     int kickCount = 0;  
 
+    //method for reassigning colours when shape resets
     public float[] reassignColours(float[] clrArr)
     {
         float[] colours = new float[]{random(0, 255),random(0, 255),random(0, 255)};
@@ -34,21 +36,28 @@ public class AdamVisual extends Visual {
         mainVisual.textAlign(CENTER,CENTER);
         mainVisual.beat.detect(mainVisual.as.mix);
         mainVisual.beat.detectMode(0);
+        //counting framerate
         mainVisual.fCounter++;
+        //translate so that the centre of the screen is (0, 0)
         mainVisual.translate(mainVisual.width/2, mainVisual.height/2);
 
+        //When a kick is detected in song, increment variable
         if (mainVisual.beat.isKick())
         {
             kickCount++; 
         }
 
+        //fill shapes with colour
         mainVisual.fill(clrArr[0], clrArr[1], clrArr[2], 40);
+        //increase radius of shape with kickCount
         mainVisual.eRadius = 5*kickCount;
-            
+        
+        //Default visual (circle) - KeyCode 1
         if (mainVisual.adamOption == 1)
         {
+            //Draw circle
             mainVisual.ellipse(0, 0, mainVisual.eRadius, mainVisual.eRadius);
-            mainVisual.eRadius*=0.95;
+            //Reset circle once it hits boundary
             if ( mainVisual.eRadius > ((float)mainVisual.width)/2 ) 
             {
                 for (int i=0; i < 3; i++)
@@ -61,13 +70,16 @@ public class AdamVisual extends Visual {
                 
             }
         }
+        // Rectangle Visual - KeyCode 2
         if (mainVisual.adamOption == 2)
         {
+            //Draw rectangles 
             mainVisual.fill(clrArr[0], clrArr[1], clrArr[2], 25);
             mainVisual.rect(-mainVisual.width/2, -mainVisual.height/2, mainVisual.eRadius, mainVisual.eRadius);
             mainVisual.rect(mainVisual.width/2, -mainVisual.height/2, -mainVisual.eRadius, mainVisual.eRadius);
             mainVisual.rect(-mainVisual.width/2, mainVisual.height/2, mainVisual.eRadius, -mainVisual.eRadius);
             mainVisual.rect(mainVisual.width/2, mainVisual.height/2, -mainVisual.eRadius, -mainVisual.eRadius);
+            //reset when boundary hit
             if ( mainVisual.eRadius > ((float)mainVisual.width)*1.1 ) 
             {
                 for (int i=0; i < 3; i++)
@@ -81,12 +93,15 @@ public class AdamVisual extends Visual {
             }
             
         }
+        // Hourglass visual - KeyCode 3
         if (mainVisual.adamOption == 3)
         {
             mainVisual.pushMatrix();
 
+            //Rotate shape around centre of screen
             mainVisual.rotate( MainVisual.map(mainVisual.fCounter%360, 0, 360, 0, MainVisual.PI*2));
             
+            //Draw triangles
             mainVisual.triangle(0, -100, 60+(float)0.6*mainVisual.eRadius, mainVisual.eRadius, (-60-(float)0.6*mainVisual.eRadius), mainVisual.eRadius);
             mainVisual.triangle(0, 100, 60+(float)0.6*mainVisual.eRadius, mainVisual.eRadius, (-60-(float)0.6*mainVisual.eRadius), mainVisual.eRadius);
             mainVisual.triangle(-100, 0, 60+(float)0.6*mainVisual.eRadius, mainVisual.eRadius, (-60-(float)0.6*mainVisual.eRadius), mainVisual.eRadius);
@@ -99,7 +114,7 @@ public class AdamVisual extends Visual {
 
             mainVisual.popMatrix();
             
-    
+            //Reset when boundary hit
             if ( mainVisual.eRadius > ((float)mainVisual.width)*0.7 ) 
             {
                 for (int i=0; i < 3; i++)
@@ -113,9 +128,12 @@ public class AdamVisual extends Visual {
             
         }
         
+        //Draw rectangle for background 
         mainVisual.fill(prevClrs[0], prevClrs[1], prevClrs[2], 25);
         mainVisual.rect(-(mainVisual.width/2)-2, -mainVisual.height/2, mainVisual.height*2, mainVisual.width*2);
 
+        //Spiral that is in centre of screen for all visuals
+        //space variable makes shape expand and retract with amplitude of the song
         float space = mainVisual.getSmoothedAmplitude();       
         mainVisual.stroke(PApplet.map(mainVisual.getAmplitude(), 0, 1, 0, 255), 255, 255);
         mainVisual.rotate( MainVisual.map(mainVisual.fCounter%360, 0, 360, 0, MainVisual.PI*2));
