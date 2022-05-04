04fca51ad0443627e1a3f9eda8d9bdfd85c58386
GonzoTheDev <c20703429@mytudublin.ie>
2022-05-04 22:43:06 +0100

    Update Visual.java

--- java/src/ie/tudublin/Visual.java
+++ java/src/ie/tudublin/Visual.java
@@ -6,7 +6,7 @@ import ddf.minim.analysis.*;
 
 public abstract class Visual extends PApplet
 {
-	private int frameSize = 2048;
+	private int frameSize = 1024;
 	private int sampleRate = 44100;
 
 	private float[] bands;
